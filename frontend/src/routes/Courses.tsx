082c4876b8e3d245adfcde814a52692b53724331
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-11-05 11:06:12 +0900

    Implement a button to delete all localstrage data in local strage

--- frontend/src/routes/Courses.tsx
+++ frontend/src/routes/Courses.tsx
@@ -13,72 +13,57 @@ const Courses = (): JSX.Element => {
             navigate("/question")
         }
     ;
+    const choiceClass = () => {
+
+    }
+    const choiceCategory = () => {
+
+    }
     return (<>
         <div className="flex flex-col min-h-screen">
             <main className="flex-grow">
                 <h3>( ..)φ 漢検練習帳</h3>
 
-                <div>
-                    <p className="text-xl flex justify-center m-5">級を選んでください</p>
-
-                    <label>
-                        <input type="radio" name="radio-1"/>
-                        <span className="button">準1級</span>
-                    </label>
-                    <label>
-                        <input type="radio" name="radio-1"/>
-                        <span className="button">1級</span>
-                    </label>
-                </div>
-
-                <div>
-                    <div>
-                        <p className="text-xl flex justify-center m-5">分野を選んでください</p>
-                        <label>
-                            <input type="radio" name="radio-2"/>
-                            <span className="button">全分野</span>
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            <input type="radio" name="radio-2"/>
-                            <span className="button">読み</span>
-                        </label>
-                        <label>
-                            <input type="radio" name="radio-2"/>
-                            <span className="button">書き</span>
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            <input type="radio" name="radio-2"/>
-                            <span className="button">四字熟語（書き）</span>
-                        </label>
-                        <label>
-                            <input type="radio" name="radio-2"/>
-                            <span className="button">四字熟語（読み）</span>
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            <input type="radio" name="radio-2"/>
-                            <span className="button">故事成語・諺</span>
-                        </label>
-                        <label>
-                            <input type="radio" name="radio-2"/>
-                            <span className="button">対義語・類義語</span>
-                        </label>
-                    </div>
-
-                </div>
-                <p className="text-xl flex justify-center m-5 ">新規又は復習を選んでください</p>
+                <p className="text-xl flex justify-center m-1">級を選んでください</p>
                 <div className="flex justify-center">
-
-
-                    <button className="btn btn-wide m-3 btn-primary" onClick={allQuizStart}>新規</button>
-                    <button className="btn btn-wide m-3 btn-primary" onClick={repeatQuizStart}>復習</button>
+                    <button className="btn btn-wide m-2 btn-primary" onClick={choiceClass}>準1級</button>
+                    <button className="btn btn-wide m-2 btn-primary" onClick={choiceClass}>1級</button>
                 </div>
-                <p className="text-l flex justify-center m-5">選んだ分野のすべての問題から出題されます 「復習する」にチェックした問題から出題されます</p>
+
+                <div>
+                    <div>
+                        <p className="text-xl flex justify-center m-2">分野を選んでください</p>
+                        <div className="flex justify-center">
+                            <button className="btn btn-wide m-2 btn-primary" onClick={choiceCategory}>全分野</button>
+
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="btn btn-wide m-2 btn-primary" onClick={choiceCategory}>読み</button>
+                            <button className="btn btn-wide m-2 btn-primary" onClick={choiceCategory}>書き</button>
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="btn btn-wide m-2 btn-primary" onClick={choiceCategory}>四字熟語（書き）</button>
+                            <button className="btn btn-wide m-2 btn-primary" onClick={choiceCategory}>四字熟語（読み）</button>
+                        </div>
+                        <div className="flex justify-center">
+                            <button className="btn btn-wide m-2 btn-primary" onClick={choiceCategory}>故事成語・諺</button>
+                            <button className="btn btn-wide m-2 btn-primary" onClick={choiceCategory}>対義語・類義語</button>
+                        </div>
+
+                        <p className="text-xl flex justify-center m-2 ">新規又は復習を選んでください</p>
+                        <div className="flex justify-center">
+
+
+                            <button className="btn btn-wide m-2 btn-primary" onClick={allQuizStart}>新規</button>
+                            <button className="btn btn-wide m-2 btn-primary" onClick={repeatQuizStart}>復習</button>
+                        </div>
+                        <p className="text-xs flex justify-center">選んだ分野のすべての問題から出題されます 「復習する」にチェックした問題から出題されます</p>
+
+                        <div className="grid grid-flow-col gap-4 link flex justify-end m-5">
+                            <Link to="/vite-dev">トップ画面に戻る</Link>
+                        </div>
+                    </div>
+                </div>
 
             </main>
 
--- frontend/src/routes/Top.tsx
+++ frontend/src/routes/Top.tsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import Footer from "../components/Footer";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const Top = (): JSX.Element => {
+    const navigate = useNavigate()
+    const start = () => {
+        navigate("/courses")
+    }
+    const deleteAll = () => {
+        localStorage.removeItem("repeatItemID")
+    }
+
     return (<>
         <div className="flex flex-col min-h-screen">
             <main className="flex-grow">
                 <h1 className="text-3xl ">( ..)φ 漢検練習帳</h1>
 
-                <h3 className="text-xl">漢字検定1級・準1級範囲の漢字を 4択クイズで学習するアプリです</h3>
-                <p>（1級は準備中です）</p>
-
-                <Link to="/courses">学習する</Link>
-                <div>
-                    <button>復習データを消す</button>
+                <h3 className="text-xl flex justify-center m-10">漢字検定1級・準1級範囲の漢字を 4択クイズで学習するアプリです</h3>
+                <p className="flex justify-center">（1級は準備中です）</p>
+                <div className="flex justify-center m-8">
+                    <button className="btn btn-wide btn-primary  text-2xl m-10"
+                            onClick={start}>始める
+                    </button>
                 </div>
-                <p>~~~~</p>
-                <p>説明画像📸</p>
-                <p>~~~~</p>
+                <div className="flex justify-end m-3">
+                    <button className="btn btn-active btn-link" onClick={deleteAll}>復習データを消す</button>
+                </div>
+                <p className="flex justify-center">~~~~</p>
+                <p className="flex justify-center">説明画像📸</p>
+                <p className="flex justify-center">~~~~</p>
+
             </main>
 
             <Footer/></div>
