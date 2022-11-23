8341899eea4912b8b618d8fc3b39fbc1d2b8961d
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-11-24 00:40:37 +0900

    Pass eslint in API.tsx

--- frontend/src/API.tsx
+++ frontend/src/API.tsx
@@ -1,13 +1,13 @@
-import axios, {AxiosError} from 'axios'
+import axios from 'axios'
 
 type ResponseData = {
-    current: string
+  current: string
 }
 
 export const useFetchApi = () => {
-    const fetchRequest = async(level: string, category: string, limit: string) => {
-        const response =  await axios.get<ResponseData>(`http://localhost:3000/?&category=${category}&level=${level}&limit=${limit}`)
-        return {quiz: response.data, error: null, loading: false}
-    }
-    return {fetchRequest}
+  const fetchRequest = async (level: string, category: string, limit: string) => {
+    const response = await axios.get<ResponseData>(`http://localhost:3000/?&category=${category}&level=${level}&limit=${limit}`)
+    return { quiz: response.data, error: null, loading: false }
+  }
+  return { fetchRequest }
 }
