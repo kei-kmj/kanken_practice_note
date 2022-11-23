bc47508d692ec4f3d589692a01729914ddd65f08
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-11-24 00:23:41 +0900

    Pass eslint in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,29 @@
-import {Routes, Route, Link, useLocation} from 'react-router-dom'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Qualification from './routes/Qualification'
-import Top from "./routes/Top";
+import Top from './routes/Top'
 import Support from './routes/Support'
-import Courses from "./routes/Courses";
-import Quiz from "./routes/Quiz";
-import background from "../public/shippou.jpeg"
-import Nomatch from "./routes/Nomatch";
+import Courses from './routes/Courses'
+import Quiz from './routes/Quiz'
+import background from '../public/shippou.jpeg'
+import './App.css'
 
-import "./App.css"
-
-
-function App() {
-
-
-    return (
-        <>
-            <div className="bg-auto" style={{backgroundImage: `url(${background}`}}>
-                <div className="">
-                    <Routes>
-                        <Route exact path="/" element={<Top/>}/>
-                        <Route path="/vite-dev" element={<Top/>}/>
-                        <Route path="/courses" element={<Courses/>}/>
-                        <Route path="/qualification" element={<Qualification/>}/>
-                        <Route path="/support" element={<Support/>}/>
-                        <Route path="/quiz" element={<Quiz/>}/>
-                    </Routes>
-                </div>
-            </div>
-        </>
-    )
+function App () {
+  return (
+      <>
+        <div className="bg-auto" style={ { backgroundImage: `url(${background}` } }>
+          <div className="">
+            <Routes>
+              <Route exact path="/" element={ <Top/> }/>
+              <Route path="/vite-dev" element={ <Top/> }/>
+              <Route path="/courses" element={ <Courses/> }/>
+              <Route path="/qualification" element={ <Qualification/> }/>
+              <Route path="/support" element={ <Support/> }/>
+              <Route path="/quiz" element={ <Quiz/> }/>
+            </Routes>
+          </div>
+        </div>
+      </>
+  )
 }
-
 export default App
