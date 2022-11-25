0b75729a5893a924f5f41ed0b3a35f43eb23473e
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-11-25 18:01:01 +0900

    Refactor repeatQuizStart function

--- frontend/src/routes/Courses.tsx
+++ frontend/src/routes/Courses.tsx
@@ -5,9 +5,10 @@ import Footer from '../components/Footer'
 import { useFetchApi } from '../API'
 import '../App.css'
 import logo from '../../public/logo5.png'
+import { LOCAL_STORAGE_DATA } from './Top'
 
 const Courses = () => {
-  const { fetchRequest } = useFetchApi()
+  const {fetchRequest} = useFetchApi()
   const navigate = useNavigate()
   const [level, setLevel] = useState('')
   const [category, setCategory] = useState('')
@@ -21,7 +22,7 @@ const Courses = () => {
   }
 
   const allQuizStart = async () => {
-    const { quiz } = await fetchRequest(level, category, '10')
+    const {quiz} = await fetchRequest(level, category, '10')
     console.log(quiz)
 
     if (level === '') {
@@ -34,7 +35,7 @@ const Courses = () => {
       alert('出題できる問題がありません')
       return
     }
-    navigate('/quiz', { state: { quiz } })
+    navigate('/quiz', {state: {quiz}})
   }
 
   const repeatQuizStart = () => {
@@ -46,14 +47,14 @@ const Courses = () => {
       return
     }
     const quiz = []
-    for (let i = 1; i <= 84; i++) {
-      if (!JSON.parse(localStorage.getItem(`repeatItemID${i}`))) {
+    for (let key in localStorage) {
+      if (!key.startsWith(LOCAL_STORAGE_DATA.KEY)) {
         continue
-      } else if (level !== JSON.parse(localStorage.getItem(`repeatItemID${i}`)).level.toString()) {
+      } else if (level !== JSON.parse(localStorage.getItem(key)).level.toString()) {
         continue
       } else if (category === '0' ||
-          category === JSON.parse(localStorage.getItem(`repeatItemID${i}`)).category_id.toString()) {
-        quiz.push(JSON.parse(localStorage.getItem(`repeatItemID${i}`)))
+        category === JSON.parse(localStorage.getItem(key)).category_id.toString()) {
+        quiz.push(JSON.parse(localStorage.getItem(key)))
       }
     }
     console.log(quiz)
@@ -61,7 +62,7 @@ const Courses = () => {
       alert('登録されている問題がありません')
       return
     }
-    navigate('/quiz', { state: { quiz } })
+    navigate('/quiz', {state: {quiz}})
   }
   return (<>
     <div>
@@ -69,18 +70,18 @@ const Courses = () => {
         <div className="flex justify-left items-center m-2">
           {/* <h1 className="text-3xl">漢検練習帳</h1> */}
           {/* <img className="w-10" src={chara} alt={chara}/> */}
-          <img className="w-40" src={ logo } alt={ logo }/>
+          <img className="w-40" src={logo} alt={logo}/>
         </div>
 
         <p className="text-xl flex justify-center mt-1">級を選んでください</p>
         <div className="flex justify-center m-0">
           <input id="radio1" className="radiobutton" name="level" hidden type="radio"
-                 onClick={ () => selectLevel('11') }
+                 onClick={() => selectLevel('11')}
           />
           <label htmlFor="radio1">準1級</label>
 
           <input id="radio2" className="radiobutton" name="level" hidden type="radio" value="1"
-                 onClick={ () => selectLevel('1') }/>
+                 onClick={() => selectLevel('1')}/>
           <label htmlFor="radio2">1 級</label>
         </div>
 
@@ -90,59 +91,59 @@ const Courses = () => {
             <div className="flex justify-center m-0">
               <input id="category1" className="radiobutton" name="category" hidden type="radio"
                      value="11"
-                     onClick={ () => selectCategory('0') }/>
+                     onClick={() => selectCategory('0')}/>
               <label htmlFor="category1">全分野</label>
 
               <input id="category2" className="radiobutton" name="category" hidden type="radio"
                      value="3"
-                     onClick={ () => selectCategory('3') }/>
+                     onClick={() => selectCategory('3')}/>
               <label htmlFor="category2">書 き</label>
             </div>
             <div className="flex justify-center">
               <input id="category3" className="radiobutton" name="category" hidden type="radio"
                      value="1"
-                     onClick={ () => selectCategory('1') }/>
+                     onClick={() => selectCategory('1')}/>
               <label htmlFor="category3">読 み</label>
 
               <input id="category4" className="radiobutton" name="category" hidden type="radio"
                      value="2"
-                     onClick={ () => selectCategory('2') }/>
+                     onClick={() => selectCategory('2')}/>
               <label htmlFor="category4">表外の読み</label>
 
             </div>
             <div className="flex justify-center">
               <input id="category5" className="radiobutton" name="category" hidden type="radio"
                      value="4"
-                     onClick={ () => selectCategory('4') }/>
+                     onClick={() => selectCategory('4')}/>
               <label htmlFor="category5">四字熟語（書き）</label>
 
               <input id="category6" className="radiobutton" name="category" hidden type="radio"
                      value="5"
-                     onClick={ () => selectCategory('5') }/>
+                     onClick={() => selectCategory('5')}/>
               <label htmlFor="category6">四字熟語（意味）</label>
 
             </div>
             <div className="flex justify-center">
               <input id="category7" className="radiobutton" name="category" hidden type="radio"
                      value="4"
-                     onClick={ () => selectCategory('6') }/>
+                     onClick={() => selectCategory('6')}/>
               <label htmlFor="category7">対義語・類義</label>
 
               <input id="category8" className="radiobutton" name="category" hidden type="radio"
                      value="5"
-                     onClick={ () => selectCategory('7') }/>
+                     onClick={() => selectCategory('7')}/>
               <label htmlFor="category8">故事成語・諺</label>
             </div>
 
             <p className="text-xl flex justify-center mt-5">新規又は復習を選んでください</p>
             <div className="flex justify-center">
               <div>
-                <button className="btn btn-wide m-2 btn-primary b" onClick={ allQuizStart }>新 規
+                <button className="btn btn-wide m-2 btn-primary b" onClick={allQuizStart}>新 規
                 </button>
                 <p className="flex justify-center">選んだ分野のすべての問題から出題されます</p>
               </div>
               <div>
-                <button className="btn btn-wide m-2 btn-primary b" onClick={ repeatQuizStart }>復 習
+                <button className="btn btn-wide m-2 btn-primary b" onClick={repeatQuizStart}>復 習
                 </button>
                 <p className="flex justify-center">「復習する」にチェックした問題から出題されます</p>
               </div>
