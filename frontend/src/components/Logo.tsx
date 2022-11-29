b1c72e448ba44871ae211f0245971c332ed679b1
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-11-29 14:07:43 +0900

    Cut the logo into component

---
+++ frontend/src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import logo from '../../public/logo5.png'
+import * as React from 'react'
+
+export function Logo () {
+  return <div className="flex justify-left items-center m-2">
+    {/* <img className="w-10" src={chara} alt={chara}/> */}
+    <img className="w-40" src={logo} alt={logo}/>
+  </div>
+}
