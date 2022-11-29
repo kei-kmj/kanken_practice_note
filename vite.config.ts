f4b1061d5ca04f34574ca2af2b7f3df1e9779624
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-11-29 14:18:04 +0900

    Introduce vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [RubyPlugin(), react()]
+  plugins: [RubyPlugin(), react()],
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  },
 })
