171abe84154fa560d4ca21ea9a18ebfc80cc0822
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-12-10 14:54:03 +0900

    Install cypress

---
+++ cypress/e2e/courses_page.cy.ts
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('coursesページ', () => {
+  before(() => {
+    cy.visit('/')
+
+    cy.contains('始める').click()
+    cy.url().should('include', '/courses')
+  })
+
+  it('初期状態は級のみ表示', () => {
+
+    cy.contains('読 み').should('not.exist')
+    cy.contains('新 規').should('not.exist')
+    cy.contains('準1級').click()
+  })
+  it('級選択後に分野ボタンが出る', () => {
+    cy.contains('読 み').should('exist')
+    cy.contains('読 み').click()
+
+  })
+  it('分野選択後に新規/復習ボタンが出る', () => {
+    cy.contains('新 規').should('exist')
+  })
+
+  it('復習データが無い時', () => {
+    cy.contains('復 習').click()
+
+  })
+})
+
+export {}
