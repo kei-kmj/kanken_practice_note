fda658b39ec84b9af8fa79820735db5873b43411
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-12-13 18:36:18 +0900

    Insert inquiry form

--- frontend/src/routes/Support.tsx
+++ frontend/src/routes/Support.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 const Support = () => {
   return (
     <>
-      <h1 className="text-4xl">Google Form</h1>
-      <div className="grid grid-flow-col gap-4 link text-4xl m-5">
-        <Link to="/vite-dev">トップ画面に戻る</Link>
+      <div className="flex flex-col min-h-screen">
+        <div className="flex justify-center flex-1 top-0 m-10">
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSd-Vprh4DyE9e_8UwPtZWIaT2iYdMXQ6Dpof8MudPfYvYElCw/viewform?embedded=true"
+            width="640" height="796" frameBorder="0">読み込んでいます…
+          </iframe>
+        </div>
+        <div className="flex justify-end m-8 link">
+          <Link to="/vite-dev">トップ画面に戻る</Link>
+        </div>
+        <Footer/>
       </div>
     </>
   )
