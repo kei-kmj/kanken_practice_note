7701e829fd4b3dfeb65f443b2cb88c36bd29afdc
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-01-01 15:39:38 +0900

    fix:resetting eslint

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import RubyPlugin from 'vite-plugin-ruby'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [RubyPlugin(), react()],
-  test: {},
+  plugins: [RubyPlugin(), react()]
 })
