b0975e954628583462fdd8cbea75ae4f1fddf431
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-01-03 15:46:44 +0900

    change:change images

--- frontend/src/components/quiz/Question.tsx
+++ frontend/src/components/quiz/Question.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react'
+import question from '../../../public/question.png'
 
 export const Question = (props: { currentQuiz: number, data: { quiz: string | any[] } }) => {
   return (<>
-    <div className="card quiz mx-auto items-center image-full mt-24">
+    <div className="card quiz mx-auto items-center image-full mt-12">
       <div className="indicator">
         <div className="indicator-item indicator-top">
           <p className="level">{props.data.quiz[props.currentQuiz].level === 11 ? '準1級' : '1級'}</p>
         </div>
-        {/*<figure><img className="w-96" src={frame} alt={frame}/></figure>*/}
+        <div className="indicator-item indicator-middle indicator-start">
+          <img className="w-36" src={question} alt={question} />
+        </div>
+
         <div className="card-body quiz">
-        {/*<span*/}
-        {/*  className="flex justify-end badge badge-lg text-xl mr-3">{props.data.contents[props.currentQuiz].level === 11 ? '準1級' : '1級'}</span>*/}
+
           <p className="flex justify-end mb-8">{props.currentQuiz + 1}問目 / {props.data.quiz.length} 問中</p>
 
           <p className="text-2xl m-3 flex justify-center">{props.data.quiz[props.currentQuiz].category.description}</p>
---
+++ frontend/src/pages/Top.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react'
+import { Footer } from '../components/shared/Footer'
+import { useNavigate } from 'react-router-dom'
+import logo from '../../public/logo.png'
+import icon from '../../public/icon.png'
+import './top.css'
+
+export const LOCAL_STORAGE_DATA = {
+  KEY: 'kankenApp_repeatItemID'
+}
+
+export const Top:React.FC = () => {
+  const navigate = useNavigate()
+  const start = () => {
+    navigate('/courses')
+  }
+  const deleteAll = () => {
+    for (const key in localStorage) {
+      if (key.startsWith(LOCAL_STORAGE_DATA.KEY)) {
+        localStorage.removeItem(key)
+      }
+    }
+  }
+
+  return (<>
+    <div className="flex flex-col min-h-screen sticky top-0">
+      <main className="flex-grow">
+        <div className="flex justify-center items-center m-20">
+           <img className="w-16" src={icon} alt={icon} />
+          <img className="w-96" src={logo} alt={logo} />
+        </div>
+        <h3 className="text-xl flex justify-center m-10">漢字検定1級・準1級範囲の漢字を 4択クイズで学習するアプリです</h3>
+        <p className="flex justify-center">（1級は準備中です）</p>
+        <div className="flex justify-center m-8">
+          <button className="btn btn-wide btn-primary  text-2xl m-10"
+                  onClick={start}>始める
+          </button>
+        </div>
+        <div className="flex justify-center">
+          <div className="card w-52">
+            <div className="card-body">
+              <p>クイズで漢字学習！</p>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+
+            </div>
+          </div>
+          <div className="card w-52 ml-2">
+            <div className="card-body">
+              <p>得点をチェック！</p>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+
+            </div>
+          </div>
+          <div className="card w-52 ml-2">
+            <div className="card-body">
+              <p>苦手な問題を復習！</p>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-end">
+          <label htmlFor="my-modal" className="btn text-gray-400 btn-link m-8">復習データを消す</label>
+
+          <input type="checkbox" id="my-modal" className="modal-toggle"/>
+          <div className="modal">
+            <div className="modal-box">
+              <p className="font-bold text-lg">本当に復習データを消しますか？</p>
+              <p className="font-bold text-lg">一度消すと元に戻せません</p>
+              <div className="modal-action">
+
+                <label htmlFor="my-modal" className="btn btn-info" onClick={deleteAll}>はい</label>
+                <label htmlFor="my-modal" className="btn btn-info">いいえ</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer/></div>
+  </>)
+}
