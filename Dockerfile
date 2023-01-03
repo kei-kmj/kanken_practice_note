30b3eb895282c50b59ade208e3b2f18dd8dca32a
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-01-03 12:26:49 +0900

    update:deploy in production mode

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM ruby:3.1.1
 
+ENV RAILS_ENV="production"
+ENV NODE_ENV="production"
+
 # Cloud Run default port 8080
 EXPOSE 8080
 
@@ -10,29 +13,37 @@ RUN apt-get update \
     && apt-get -y clean \
     && rm -rf /var/lib/apt/lists/*
 
-#ADD https://github.com/benbjohnson/litestream/releases/download/v0.3.9/litestream-v0.3.9-linux-amd64-static.tar.gz /tmp/litestream.tar.gz
+ADD https://github.com/benbjohnson/litestream/releases/download/v0.3.9/litestream-v0.3.9-linux-amd64-static.tar.gz /tmp/litestream.tar.gz
 
-#RUN tar -C /usr/local/bin -xzf /tmp/litestream.tar.gz
+RUN tar -C /usr/local/bin -xzf /tmp/litestream.tar.gz
 
 WORKDIR /app
 
 COPY . /app
 
+
+
 #COPY Gemfile /app/Gemfile
 #COPY Gemfile.lock /app/Gemfile.lock
 RUN bundle install
+
+RUN SECRET_KEY_BASE=placeholder bundle exec rails assets:precompile \
+ && rm -rf node_modules tmp/cache
 #
 #COPY package*.json ./
-RUN npm install vite
+#RUN npm install vite
+#RUN bin/rails assets:precompile
+#RUN npm run build
 
 # Copy Litestream configuration file & startup script.
-#COPY ./litestream.yml /etc/litestream.yml
+COPY ./litestream.yml /etc/litestream.yml
 COPY entrypoint.sh /app/entrypoint.sh
 
 RUN chmod +x /app/entrypoint.sh
 
+ENV RAILS_SERVE_STATIC_FILES="true"
 ENTRYPOINT ["/app/entrypoint.sh"]
-#CMD ["rails", "server", "-b", "0.0.0.0"]
+#CMD ["rails", "server", "-b", "0.0.0.0","-p","8080"]
 
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 set -e
 
-#if [ -f /app/db/development.sqlite3 ]; then
-#  mv /app/db/development.sqlite3 /app/db/development.sqlite3.bk
-#fi
-#
-## Restore the database
-#litestream restore -v -if-replica-exists -config /etc/litestream.yml /app/db/development.sqlite3
-#
-#if [ -f /app/db/development.sqlite3]; then
-#  echo "---- Restored from Cloud Storage ----"
-#  rm /app/db/development.sqlite3.bk
-#else
-#  # 初回起動時にはレプリカが未作成であり、リストアに失敗するので、
-#  # その場合には、冒頭でリネームしたdbファイルを元の名前に戻す
-#  echo "---- Failed to restore from Cloud Storage ----"
-#  mv /app/db/development.sqlite3.bk /app/db/development.sqlite3
-#fi
+if [ -f /app/db/development.sqlite3 ]; then
+  mv /app/db/development.sqlite3 /app/db/development.sqlite3.bk
+fi
+
+# Restore the database
+litestream restore -v -if-replica-exists -config /etc/litestream.yml /app/db/development.sqlite3
+
+if [ -f /app/db/development.sqlite3]; then
+  echo "---- Restored from Cloud Storage ----"
+  rm /app/db/development.sqlite3.bk
+else
+  # 初回起動時にはレプリカが未作成であり、リストアに失敗するので、
+  # その場合には、冒頭でリネームしたdbファイルを元の名前に戻す
+  echo "---- Failed to restore from Cloud Storage ----"
+  mv /app/db/development.sqlite3.bk /app/db/development.sqlite3
+fi
 #rails db:migrate
 
 # Run litestream with your app as the subprocess.
 #rails server -p 3000 -b 0.0.0.0
 #sqlite3 development.sqlite3
 
-#rm -f /app/tmp/pids/server.pid
+rm -f /app/tmp/pids/server.pid
 #bin/vite build --clear --mode=development
-
-echo "fogefogefoge"
-ls -l /app/db/
-echo "pppppppppp"
-cat /app/db/development.sqlite3
-echo "qqqqqqqqqq"
-rails server -p 8080 -b 0.0.0.0
-# bin/vite build --clear --mode=development
+#echo "fogefogefoge"
+#ls -l /app/db/
+#echo "pppppppppp"
+#cat /app/db/development.sqlite3
+#echo "qqqqqqqqqq"
+exec litestream replicate -exec "rails server -p 8080 -b 0.0.0.0"
