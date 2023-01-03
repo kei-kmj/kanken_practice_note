410e547994179503382fc1ada138870ab81a0539
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-01-03 16:06:59 +0900

    change:add images in quiz

---
+++ frontend/src/components/quiz/DisplayCorrect.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import correct from '../../../public/correct.png'
+
+export const DisplayCorrect = () => {
+  return <figure><img className="w-36" src={correct} alt={correct}/></figure>
+}
---
+++ frontend/src/components/quiz/DisplayIncorrect.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import incorrect from '../../../public/incorrect.png'
+
+export const DisplayIncorrect = () => {
+  return <figure><img className="w-36" src={incorrect} alt={incorrect}/></figure>}
