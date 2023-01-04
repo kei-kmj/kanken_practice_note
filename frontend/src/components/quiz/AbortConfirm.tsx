52d04bce708a31a846ce3ee46e3fb372f752a410
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-01-04 17:06:13 +0900

    fix: pass eslint in AbortConfirm.tsx

--- frontend/src/components/quiz/AbortConfirm.tsx
+++ frontend/src/components/quiz/AbortConfirm.tsx
@@ -1,21 +1,27 @@
 import * as React from 'react'
+import { FC } from 'react'
+
+type Props = {
+  answer: { id: string }
+  onClick: () => void
+}
 
 
-export const AbortConfirm = (props: { answer: { id?: string; answer?: string; correctness: boolean }, onClick: () => void }) => {
+export const AbortConfirm:FC<Props> = ({ answer: { id }, onClick }) => {
   return (<>
     <div className="flex justify-end">
-      <label htmlFor={`break${props.answer.id}`}
+      <label htmlFor={`break${id}`}
              className="btn text-gray-400 btn-link">問題選択画面に戻る</label>
 
-      <input type="checkbox" id={`break${props.answer.id}`} className="modal-toggle"/>
+      <input type="checkbox" id={`break${id}`} className="modal-toggle"/>
       <div className="modal">
         <div className="modal-box">
           <h3 className="font-bold text-lg">本当に問題選択画面に戻りますか？</h3>
           <div className="modal-action">
 
-            <label htmlFor={`break${props.answer.id}`} className="btn btn-info"
-                   onClick={props.onClick}>はい</label>
-            <label htmlFor={`break${props.answer.id}`}
+            <label htmlFor={`break${id}`} className="btn btn-info"
+                   onClick={onClick}>はい</label>
+            <label htmlFor={`break${id}`}
                    className="btn btn-info">いいえ</label>
           </div>
         </div>
