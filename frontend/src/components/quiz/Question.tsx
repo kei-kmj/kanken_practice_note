32cf9eb986cabc838f35daee81d28f927b9349e3
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-01-04 18:08:56 +0900

    fix: pass eslint in Questiontsx

--- frontend/src/components/quiz/Question.tsx
+++ frontend/src/components/quiz/Question.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react'
 import question from '../../../public/question.png'
+import { LEVEL } from '../../constants'
+import { FC } from 'react'
 
-export const Question = (props: { currentQuiz: number, data: { quiz: string | any[] } }) => {
+type Props = {
+  currentQuiz: number
+  data: { quiz: string | string[] }
+}
+
+export const Question:FC<Props> = ({ currentQuiz, data: { quiz } }) => {
   return (<>
     <div className="card quiz mx-auto items-center image-full mt-12">
       <div className="indicator">
         <div className="indicator-item indicator-top">
-          <p className="level">{props.data.quiz[props.currentQuiz].level === 11 ? '準1級' : '1級'}</p>
+          <p className="level">{quiz[currentQuiz].level.toString() === LEVEL.SEMI_FIRST ? '準1級' : '1級'}</p>
         </div>
         <div className="indicator-item indicator-middle indicator-start">
           <img className="w-36" src={question} alt={question} />
@@ -14,10 +21,10 @@ export const Question = (props: { currentQuiz: number, data: { quiz: string | an
 
         <div className="card-body quiz">
 
-          <p className="flex justify-end mb-8">{props.currentQuiz + 1}問目 / {props.data.quiz.length} 問中</p>
+          <p className="flex justify-end mb-8">{currentQuiz + 1}問目 / {quiz.length} 問中</p>
 
-          <p className="text-2xl m-3 flex justify-center">{props.data.quiz[props.currentQuiz].category.description}</p>
-          <p className="text-3xl mt-3 flex justify-center">{props.data.quiz[props.currentQuiz].question}</p>
+          <p className="text-2xl m-3 flex justify-center">{quiz[currentQuiz].category.description}</p>
+          <p className="text-3xl mt-3 flex justify-center">{quiz[currentQuiz].question}</p>
         </div>
       </div>
     </div>
