5923454d86bef0805fbfb5f1748e59ab6a629632
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-01-11 13:44:06 +0900

    fix:delete unnecessary import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
-import { Header} from './Header'
 import 'vite/modulepreload-polyfill'
 
 
