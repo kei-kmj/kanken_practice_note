150b2c1a4941a6213c3cd0079c91780dee575d3d
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-01-11 18:12:37 +0900

    delete unnecessary files

--- cypress.od/e2e/fetchapi_.cy.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-describe('API', () => {
-  it('APIを叩くと200が返る', () => {
-    cy.intercept('get', '/api/index?&category=1&level=11&limit=10',{statusCode:200})
-  })
-})
-export {}
