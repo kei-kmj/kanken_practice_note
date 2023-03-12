6ab8f5c8faf7c1d74658fbebf1ecebc1b47bec3a
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-03-13 01:21:19 +0900

    fix:change twitter card setting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,7 +23,7 @@ module ApplicationHelper
       local: 'ja-JP',
     },
       twitter: {
-      card: 'summary_large_image',
+      card: 'summary',
       site: '@kei16180339',
       image: image_url('img.png'),
     }
